502ff827d0ff73bafcf10ce0ec94d643930bbc8d
Umutcan Elçi <umutcanelci@yaani.com>
2023-01-25 00:24:36 +0300

    Solutions

---
+++ LeetCode/src/LeetSolutions/SmallestEvenMultiple/Solution.java
@@ -0,0 +1,11 @@
+package LeetSolutions.SmallestEvenMultiple;
+//2413. Smallest Even Multiple
+public class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n % 2 == 0){
+            return n;
+        }
+        return n*2;
+    }
+}
+//Well leet code accepts it but there's mistakes

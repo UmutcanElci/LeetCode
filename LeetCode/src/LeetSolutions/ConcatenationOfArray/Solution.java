5b3ea3a9485734c0f798f775b72cd95c1baf9fdb
Umutcan Elçi <umutcanelci@yaani.com>
2023-01-26 20:16:05 +0300

    485-Max-Consecutive-Ones

---
+++ LeetCode/src/LeetSolutions/ConcatenationOfArray/Solution.java
@@ -0,0 +1,9 @@
+package LeetSolutions.ConcatenationOfArray;
+//1929. Concatenation of Array
+public class Solution {
+       public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+
+           return nums;
+       }
+}

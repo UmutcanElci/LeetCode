0c1dbe90dc3f694c400de978f2b8bc35ccb96f8c
Umutcan Elçi <umutcanelci@yaani.com>
2023-01-26 22:26:07 +0300

    1929-Concatenation-Of-Array

--- LeetCode/src/LeetSolutions/ConcatenationOfArray/Solution.java
+++ LeetCode/src/LeetSolutions/ConcatenationOfArray/Solution.java
@@ -3,7 +3,10 @@ package LeetSolutions.ConcatenationOfArray;
 public class Solution {
        public int[] getConcatenation(int[] nums) {
         int n = nums.length;
-
-           return nums;
+        int[] arr = new int[n*2];
+        for (int i = 0; i<n;i++){
+            arr[i] = arr[i+n] = nums[i];
+        }
+        return arr;
        }
 }

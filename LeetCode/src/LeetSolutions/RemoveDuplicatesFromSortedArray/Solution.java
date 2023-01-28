5f93cedc71ea7067500d2b885c4234a678f090ec
Umutcan Elçi <umutcanelci@yaani.com>
2023-01-28 03:53:01 +0300

    26-Remove-Duplicates-From-Sorted-Array

---
+++ LeetCode/src/LeetSolutions/RemoveDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,18 @@
+package LeetSolutions.RemoveDuplicatesFromSortedArray;
+
+import java.util.LinkedHashSet;
+//26. Remove Duplicates from Sorted Array
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+      int j = 0;
+      for (int i = 0 ; i<nums.length-1;i++){
+          if (nums[i] != nums[i+1]){
+              nums[j] = nums[i];
+              j++;
+          }
+      }
+      nums[j] = nums[nums.length-1];
+      j++;
+      return j;
+    }
+}

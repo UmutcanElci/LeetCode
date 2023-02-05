d8a7e58de77777e5c1c3a550facd9494a48cff9c
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-05 13:36:54 +0300

    1-Two-Sum

---
+++ LeetCode/src/LeetSolutions/TwoSum/Solution.java
@@ -0,0 +1,17 @@
+package LeetSolutions.TwoSum;
+//1. Two Sum
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] arr1 = new int[2];
+        for (int i = 0 ; i<n;i++){
+            for (int j = i+1; j<n;j++){
+                if (nums[i] + nums[j] == target){
+                    arr1[0] = i;
+                    arr1[1] = j;
+                }
+            }
+        }
+        return arr1;
+    }
+}
